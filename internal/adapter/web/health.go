f9f348d4e6b8b11bd7843687bdcaaeb84c0540e4
William Sciaroni <wsciaroni@gmail.com>
2026-01-18 14:35:53 -0600

    Fix go lint

--- internal/adapter/web/health.go
+++ internal/adapter/web/health.go
@@ -2,6 +2,7 @@ package web
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -34,5 +35,9 @@ func (h *HealthHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(response)
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		// We can't write a new HTTP status code here because the header is already sent,
+		// but we should log it so we know something is wrong.
+		log.Printf("failed to encode response: %v", err)
+	}
 }
